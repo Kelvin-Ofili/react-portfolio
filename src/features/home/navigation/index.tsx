e8a51561775a9cacba8882ed4044f529ef255504
Kelvin <kelvinofili1@gmail.com>
2023-09-26 10:49:05 +0100

    some touches

--- src/features/home/navigation/index.tsx
+++ src/features/home/navigation/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
-import { GlassPanel } from "components";
+import { GlassPanel, Hamburger } from "components";
 
 function Navigation() {
+	const [clicked, setClicked] = useState(false);
+	const handleClicked = () => {
+		setClicked(!clicked);
+	};
 	return (
 		<GlassPanel>
 			<nav className={styles.navHome}>
@@ -32,6 +36,39 @@ function Navigation() {
 					Cerificates
 				</Link>
 			</nav>
+			<div
+				className={!clicked ? `${styles.phoneNavigation}` : `${styles.showNav}`}
+			>
+				<div onClick={handleClicked}>
+					<Hamburger />
+				</div>
+				<nav>
+					<Link
+						to="/about"
+						className={`btn ${styles.navHome__btn} ${styles.blackBtn}`}
+					>
+						About
+					</Link>
+					<Link
+						to="/projects"
+						className={`btn ${styles.navHome__btn} ${styles.blackBtn}`}
+					>
+						Projects
+					</Link>
+					<Link
+						to="/contact"
+						className={`btn ${styles.navHome__btn} ${styles.blackBtn}`}
+					>
+						Contact
+					</Link>
+					<Link
+						to="/certifications"
+						className={`btn ${styles.navHome__btn} ${styles.blackBtn}`}
+					>
+						Cerificates
+					</Link>
+				</nav>
+			</div>
 		</GlassPanel>
 	);
 }
